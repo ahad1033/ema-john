0b93d05e02375b1621b50607da614a254089d7a5
ahad1033 <a.ahad2232@gmail.com>
2023-03-29 01:48:49 +0600

    cart-modified

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
+    console.log(props)
     const { img, name, seller, quantity, price, ratings } = props.product;
+    const handelAddToCart = props.handelAddToCart;
+    
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +18,9 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings}</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handelAddToCart(props.product)} className='btn-cart'>Add to Cart
+            <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -5,13 +5,20 @@ import './Shop.css'
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect( ()=>{
         // fetch('products.json')
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
-    }, [])
+    }, []);
+
+    const handelAddToCart = (product) => {
+        // cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
     return (
         <div className='shop-container'>
@@ -20,11 +27,13 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product ={product}
+                        handelAddToCart = {handelAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h4>Order Summery</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
