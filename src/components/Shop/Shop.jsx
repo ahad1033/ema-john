b97cd14b21ca5ebfbb89f0c2ef16fa78a99660f9
ahad1033 <a.ahad2232@gmail.com>
2023-03-30 01:53:48 +0600

    retive-product-from-stored-cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -16,7 +16,25 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, []);
 
-    
+    useEffect( () =>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        //step 1: get id of the addedProduct
+        for(const id in storedCart){
+            //step 2: get product from products id by uning id
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                //step 3: add quantity
+                const quantity= storedCart[id];
+                addedProduct.quantity = quantity;
+                //step 4: add the added cart to the saved cart
+                savedCart.push(addedProduct);
+            }
+            // console.log('addedProduct', addedProduct);
+        }
+        //step 5: set the cart
+        setCart(savedCart);
+    }, [products])
 
     const handelAddToCart = (product) => {
         // cart.push(product);
