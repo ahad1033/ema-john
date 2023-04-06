80067001a97cad5264d92f280d43150e3a7ccbca
ahad1033 <a.ahad2232@gmail.com>
2023-04-06 18:00:21 +0600

    review-item-updated

--- src/components/reviewitem/ReviewItem.jsx
+++ src/components/reviewitem/ReviewItem.jsx
@@ -8,6 +8,12 @@ const ReviewItem = ({product}) => {
     return (
         <div className='review-item'>
             <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete'>D</button>
         </div>
     );
 };
